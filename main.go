cb225a2af45d47d01d5cdba15011cb66ca70b7b8
Mihail Marian <mihailmarian12@gmail.com>
2023-02-28 19:48:03 +0000

    wip

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"regexp"
+)
+
+func main() {
+	http.HandleFunc("/companies/sic_code/", handler)
+	fmt.Println("Starting server on localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	sic := r.URL.Path[len("/companies/sic_code/"):]
+
+	valid, err := isValidSicFormat(sic)
+	if err != nil {
+		fmt.Println("Valid SIC format check failed:", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
+
+	if !valid {
+		http.Error(w, fmt.Sprintf("Invalid SIC code: %s", sic), http.StatusBadRequest)
+	}
+
+	fmt.Fprintf(w, "SIC Code: %s", sic)
+}
+
+func isValidSicFormat(sic string) (bool, error) {
+	pattern := "^[0-9]+$"
+	match, err := regexp.MatchString(pattern, sic)
+	return match, err
+}

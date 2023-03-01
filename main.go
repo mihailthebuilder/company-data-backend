600979418d7edafe2b1670609b8ac3ded299fd3d
Mihail Marian <mihailmarian12@gmail.com>
2023-03-01 15:55:23 +0000

    wip

--- main.go
+++ main.go
@@ -1,45 +1,67 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
+
+	"github.com/gin-gonic/gin"
 )
 
-type Company struct {
-	Name   string
-	Number string
-	Url    string
-}
+var dbConn *sql.DB
 
 func main() {
-	http.HandleFunc("/companies/sic_code/", handler)
-	log.Println("Starting server on localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	connectToDatabase(dbConn)
+
+	r := gin.Default()
+
+	r.GET("/companies/sic_code/:sic_code", handleCompaniesBySicCodeRequest)
+
+	r.Run()
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	sic := r.URL.Path[len("/companies/sic_code/"):]
-
-	log.Fatal("Test")
-	log.Printf("%s %s", r.Method, "/companies/sic_code/")
+func handleCompaniesBySicCodeRequest(c *gin.Context) {
+	sic := c.Param("sic_code")
 
 	valid, err := isValidSicFormat(sic)
 	if err != nil {
 		log.Println("Valid SIC format check failed:", err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		c.JSON(http.StatusInternalServerError, "Internal server error")
 	}
 
 	if !valid {
-		http.Error(w, fmt.Sprintf("Invalid SIC code: %s", sic), http.StatusBadRequest)
+		c.JSON(http.StatusBadRequest, fmt.Sprintf("Invalid SIC code: %s", sic))
 	}
 
-	fmt.Fprintf(w, "SIC Code: %s", sic)
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
 }
 
 func isValidSicFormat(sic string) (bool, error) {
 	pattern := "^[0-9]+$"
 	match, err := regexp.MatchString(pattern, sic)
 	return match, err
+}
+
+func connectToDatabase(dbConn *sql.DB) error {
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s", GetEnv("DB_HOST"), GetEnv("DB_PORT"), GetEnv("DB_USER"), GetEnv("DB_PASSWORD"), GetEnv("DB_NAME"))
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return err
+	}
+
+	*dbConn = *db
+	return nil
+}
+
+func GetEnv(env string) string {
+	val := os.Getenv(env)
+	if val == "" {
+		log.Fatalf("Environment variable not set: %s", env)
+	}
+	return val
 }

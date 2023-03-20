f492d0a4d4ca5b670374ef3ed6747150c0787a63
Mihail Marian <mihailmarian12@gmail.com>
2023-03-20 19:54:03 +0000

    new data returned

--- main.go
+++ main.go
@@ -93,20 +93,27 @@ func isValidSicFormat(sic *string) bool {
 	return match
 }
 
-type Company struct {
+type CompanyRow struct {
 	CompanyName       string
 	CompanyNumber     string
 	AddressLine1      string
 	AddressLine2      string
 	PostTown          string
 	PostCode          string
-	CompanyStatus     string
+	AccountCategory   string
 	IncorporationDate string
 }
 
+type ProcessedCompany struct {
+	Name              string `json:"name"`
+	CompaniesHouseUrl string `json:"companiesHouseUrl"`
+	Address           string `json:"address"`
+	Size              string `json:"size"`
+	IncorporationDate string `json:"incorporationDate"`
+}
+
 func processCompaniesBySicCodeRequest(sic *string, c *gin.Context) {
-	var company Company
-	var companies []Company
+	var companies []ProcessedCompany
 
 	template := `
 	SELECT cb."CompanyName", 
@@ -115,16 +122,18 @@ func processCompaniesBySicCodeRequest(sic *string, c *gin.Context) {
 		cb."RegAddress.AddressLine2",
 		cb."RegAddress.PostTown",
 		cb."RegAddress.PostCode",
-		cb."CompanyStatus",
-		cb."IncorporationDate"
+		cb."IncorporationDate",
+		cb."Accounts.AccountCategory"
 	FROM "sic_company" sc
-	TABLESAMPLE SYSTEM (0.5)
+	TABLESAMPLE SYSTEM (10)
 	JOIN "company_base" cb
-		ON sc."SicCode" = $1
+		ON sc."SicDescription" = $1
 		AND sc."Snapshot" = '2023-03-01'
 		AND cb."CompanyNumberSnapshot" = sc."CompanyNumberSnapshot"
+		AND cb."CompanyStatus" = 'Active'
+		AND cb."Accounts.AccountCategory" != 'DORMANT'
 	ORDER BY RANDOM()
-	LIMIT 10
+	LIMIT 20
 	`
 
 	rows, err := dbConn.Query(template, *sic)
@@ -133,22 +142,69 @@ func processCompaniesBySicCodeRequest(sic *string, c *gin.Context) {
 	}
 
 	for rows.Next() {
+		var companyRow CompanyRow
+
 		err = rows.Scan(
-			&company.CompanyName,
-			&company.CompanyNumber,
-			&company.AddressLine1,
-			&company.AddressLine2,
-			&company.PostTown,
-			&company.PostCode,
-			&company.CompanyStatus,
-			&company.IncorporationDate,
+			&companyRow.CompanyName,
+			&companyRow.CompanyNumber,
+			&companyRow.AddressLine1,
+			&companyRow.AddressLine2,
+			&companyRow.PostTown,
+			&companyRow.PostCode,
+			&companyRow.IncorporationDate,
+			&companyRow.AccountCategory,
 		)
 		if err != nil {
 			log.Panic("Error scanning db row: ", err)
 		}
 
-		companies = append(companies, company)
+		processedCompany := ProcessedCompany{
+			Name:              companyRow.CompanyName,
+			CompaniesHouseUrl: fmt.Sprintf("https://find-and-update.company-information.service.gov.uk/company/%s", companyRow.CompanyNumber),
+			Address:           fmt.Sprintf("%s,%s,%s,%s", companyRow.AddressLine1, companyRow.AddressLine2, companyRow.PostTown, companyRow.PostCode),
+			IncorporationDate: companyRow.IncorporationDate,
+			Size:              getCompanySize(companyRow.AccountCategory),
+		}
+
+		companies = append(companies, processedCompany)
 	}
 
 	c.JSON(http.StatusOK, companies)
+}
+
+func getCompanySize(accountCategory string) string {
+	return AccountRankingToSize[CompanyAccountRanking[accountCategory]]
+}
+
+var AccountRankingToSize = map[int]string{
+	0: "very small",
+	1: "small",
+	2: "medium",
+	3: "large",
+}
+
+/*
+Small, unaudited abridged, total exemption full and total exemption small – has a turnover of GBP 10.2m or less, GBP 5.1m or less on its balance sheet and has 50 employees or less (#809) – two or more must apply
+Microentity – has a  turnover of GBP 632k or less, GBP 316k or less on its balance sheet and has 10 employees or less (#733) – two or more must apply
+Dormant – not doing business and doesn’t have any other income (#147)
+Full – has a turnover of above GBP 10.2m or does not satisfy two or more of the criteria required to be a micro-entity or small company (#14)
+*/
+
+var CompanyAccountRanking = map[string]int{
+	"ACCOUNTS TYPE NOT AVAILABLE": 0,
+	"AUDITED ABRIDGED":            2,
+	"AUDIT EXEMPTION SUBSIDIARY":  3,
+	"DORMANT":                     0,
+	"FILING EXEMPTION SUBSIDIARY": 3,
+	"FULL":                        3,
+	"GROUP":                       3,
+	"INITIAL":                     3,
+	"MEDIUM":                      3,
+	"MICRO ENTITY":                1,
+	"NO ACCOUNTS FILED":           0,
+	"PARTIAL EXEMPTION":           2,
+	"SMALL":                       2,
+	"TOTAL EXEMPTION FULL":        2,
+	"TOTAL EXEMPTION SMALL":       2,
+	"UNAUDITED ABRIDGED":          2,
 }

fc6f57f6d2ba29d032451258669baf41df1ff148
Mihail Marian <mihailmarian12@gmail.com>
2023-04-15 16:11:11 +0100

    wip

--- authenticate.go
+++ authenticate.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func handleAuthentication(c *gin.Context) {
-	token := generateToken()
+	token, err := generateJwtToken()
 
-	c.JSON(http.StatusOK, gin.H{"token": *token})
+	if err != nil {
+		log.Panic("Token generation error:", err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": token})
 }
 
-func generateToken() *string {
-	out := "helloworld"
-	return &out
+func generateJwtToken() (string, error) {
+	tokenLifespanInMinutes, err := strconv.Atoi(getEnv("TOKEN_MINUTE_LIFESPAN"))
+
+	if err != nil {
+		return "", fmt.Errorf("failed to parse token lifespan: %s", err)
+	}
+
+	claims := jwt.MapClaims{}
+	claims["exp"] = time.Now().Add(time.Minute * time.Duration(tokenLifespanInMinutes)).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(getEnv("API_SECRET")))
 }

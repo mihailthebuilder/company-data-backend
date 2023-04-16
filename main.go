e7a3e999c709d52f5764bb6ad735e41dbbad8c78
Mihail Marian <mihailmarian12@gmail.com>
2023-04-16 07:34:23 +0100

    wip

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"os"
 
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
@@ -25,4 +27,29 @@ func loadEnvironmentVariablesFromDotEnvFile() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+}
+
+func runServer() {
+	r := gin.Default()
+
+	serverRecoversFromAnyPanicAndWrites500(r)
+	allowAllOriginsForCORS(r)
+
+	r.POST("/companies/sample", handleRequestForCompaniesSample)
+	r.POST("/register", handleRegistration)
+
+	authorised := r.Group("/authorized", verifyAuthorization)
+	{
+		authorised.POST("/companies", handleRequestForEntireList)
+	}
+
+	r.Run()
+}
+
+func serverRecoversFromAnyPanicAndWrites500(engine *gin.Engine) {
+	engine.Use(gin.Recovery())
+}
+
+func allowAllOriginsForCORS(engine *gin.Engine) {
+	engine.Use(cors.Default())
 }

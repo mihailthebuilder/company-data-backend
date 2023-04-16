4a10f39bdcd57a711d850cd36555abe56e4f15e2
Mihail Marian <mihailmarian12@gmail.com>
2023-04-16 15:06:57 +0100

    wip

--- database.go
+++ database.go
@@ -1,8 +1,159 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"strings"
+)
+
 type IDatabase interface {
 	GetSampleListOfCompaniesForIndustry(industry *string) (*[]ProcessedCompany, error)
 }
 
 type Database struct {
+}
+
+func (d *Database) GetSampleListOfCompaniesForIndustry(industry *string) (*[]ProcessedCompany, error) {
+	conn := getDatabaseConnection()
+	defer conn.Close()
+
+	var companies []ProcessedCompany
+
+	template := `
+	SELECT cb."CompanyName", 
+		sc."CompanyNumber",
+		cb."RegAddress.AddressLine1",
+		cb."RegAddress.AddressLine2",
+		cb."RegAddress.PostTown",
+		cb."RegAddress.PostCode",
+		cb."IncorporationDate",
+		cb."Accounts.AccountCategory"
+	FROM "sic_company" sc
+	TABLESAMPLE SYSTEM (10)
+	JOIN "company_base" cb
+		ON sc."SicDescription" = $1
+		AND sc."Snapshot" = '2023-03-01'
+		AND cb."CompanyNumberSnapshot" = sc."CompanyNumberSnapshot"
+		AND cb."CompanyStatus" = 'Active'
+		AND cb."Accounts.AccountCategory" != 'DORMANT'
+	ORDER BY RANDOM()
+	LIMIT 10
+	`
+
+	rows, err := conn.Query(template, *industry)
+	if err != nil {
+		log.Panic("Query error: ", err)
+	}
+
+	for rows.Next() {
+		var companyRow CompanyRow
+
+		err = rows.Scan(
+			&companyRow.CompanyName,
+			&companyRow.CompanyNumber,
+			&companyRow.AddressLine1,
+			&companyRow.AddressLine2,
+			&companyRow.PostTown,
+			&companyRow.PostCode,
+			&companyRow.IncorporationDate,
+			&companyRow.AccountCategory,
+		)
+		if err != nil {
+			log.Panic("Error scanning db row: ", err)
+		}
+
+		processedCompany := ProcessedCompany{
+			Name:              companyRow.CompanyName,
+			CompaniesHouseUrl: fmt.Sprintf("https://find-and-update.company-information.service.gov.uk/company/%s", companyRow.CompanyNumber),
+			Address:           generateAddress(companyRow.AddressLine1, companyRow.AddressLine2, companyRow.PostTown, companyRow.PostCode),
+			IncorporationDate: companyRow.IncorporationDate,
+			Size:              calculateCompanySize(companyRow.AccountCategory),
+		}
+
+		companies = append(companies, processedCompany)
+	}
+
+	return &companies, nil
+}
+
+type CompanyRow struct {
+	CompanyName       string
+	CompanyNumber     string
+	AddressLine1      string
+	AddressLine2      string
+	PostTown          string
+	PostCode          string
+	AccountCategory   string
+	IncorporationDate string
+}
+
+type ProcessedCompany struct {
+	Name              string `json:"name"`
+	CompaniesHouseUrl string `json:"companiesHouseUrl"`
+	Address           string `json:"address"`
+	Size              string `json:"size"`
+	IncorporationDate string `json:"incorporationDate"`
+}
+
+func getDatabaseConnection() *sql.DB {
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", getEnv("DB_HOST"), getEnv("DB_PORT"), getEnv("DB_USER"), getEnv("DB_PASSWORD"), getEnv("DB_NAME"))
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Panic("Error opening database connection: ", err)
+	}
+
+	return db
+}
+
+func calculateCompanySize(accountCategory string) string {
+	return AccountRankingToSize[CompanyAccountRanking[accountCategory]]
+}
+
+func generateAddress(addressEntries ...string) string {
+	nonEmptyAddressEntries := []string{}
+
+	for _, entry := range addressEntries {
+		entryWithoutWhitespace := strings.TrimSpace(entry)
+
+		if len(entryWithoutWhitespace) > 0 {
+			nonEmptyAddressEntries = append(nonEmptyAddressEntries, entry)
+		}
+	}
+
+	return strings.Join(nonEmptyAddressEntries, ", ")
+}
+
+var AccountRankingToSize = map[int]string{
+	0: "very small",
+	1: "small",
+	2: "medium",
+	3: "large",
+}
+
+/*
+Small, unaudited abridged, total exemption full and total exemption small – has a turnover of GBP 10.2m or less, GBP 5.1m or less on its balance sheet and has 50 employees or less (#809) – two or more must apply
+Microentity – has a  turnover of GBP 632k or less, GBP 316k or less on its balance sheet and has 10 employees or less (#733) – two or more must apply
+Dormant – not doing business and doesn’t have any other income (#147)
+Full – has a turnover of above GBP 10.2m or does not satisfy two or more of the criteria required to be a micro-entity or small company (#14)
+*/
+
+var CompanyAccountRanking = map[string]int{
+	"ACCOUNTS TYPE NOT AVAILABLE": 0,
+	"AUDITED ABRIDGED":            2,
+	"AUDIT EXEMPTION SUBSIDIARY":  3,
+	"DORMANT":                     0,
+	"FILING EXEMPTION SUBSIDIARY": 3,
+	"FULL":                        3,
+	"GROUP":                       3,
+	"INITIAL":                     3,
+	"MEDIUM":                      3,
+	"MICRO ENTITY":                1,
+	"NO ACCOUNTS FILED":           0,
+	"PARTIAL EXEMPTION":           2,
+	"SMALL":                       2,
+	"TOTAL EXEMPTION FULL":        2,
+	"TOTAL EXEMPTION SMALL":       2,
+	"UNAUDITED ABRIDGED":          2,
 }
--- main_test.go
+++ main_test.go
@@ -12,18 +12,9 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-type MockEmailer struct {
-	mock.Mock
-}
-
-func (e MockEmailer) SendEmail(d *EmailDetails) error {
-	args := e.Called(d)
-	return args.Error(0)
-}
-
 func TestRegisterRoute_ShouldReturnInvalidRequestWhenFormDataNotGiven(t *testing.T) {
-	var config = RouteHandler{}
-	r := createRouter(&config)
+	var handler = RouteHandler{}
+	r := createRouter(&handler)
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("POST", "/register", nil)
@@ -33,8 +24,8 @@ func TestRegisterRoute_ShouldReturnInvalidRequestWhenFormDataNotGiven(t *testing
 }
 
 func TestRegisterRoute_ShouldReturnInvalidRequestWhenPartialFormDataGiven(t *testing.T) {
-	var config = RouteHandler{}
-	r := createRouter(&config)
+	var handler = RouteHandler{}
+	r := createRouter(&handler)
 
 	w := httptest.NewRecorder()
 
@@ -56,12 +47,12 @@ func TestRegisterRoute_ShouldReturnJwtTokenWhenFullFormDataGiven(t *testing.T) {
 	e := MockEmailer{}
 	e.On("SendEmail", &EmailDetails{EmailAddress: "hello@world.com", Title: "Company Data - Registration request", Message: fmt.Sprintf("Reason for wanting data: %s . Problem being solved: %s", body.ReasonForWantingData, body.ProblemBeingSolved)}).Return(nil)
 
-	var config = RouteHandler{
+	var handler = RouteHandler{
 		Emailer:                   e,
 		JwtTokenLifespanInMinutes: "60",
 		ApiSecret:                 "helloWorld",
 	}
-	r := createRouter(&config)
+	r := createRouter(&handler)
 
 	w := httptest.NewRecorder()
 
@@ -73,9 +64,18 @@ func TestRegisterRoute_ShouldReturnJwtTokenWhenFullFormDataGiven(t *testing.T) {
 	assert.Equal(t, http.StatusOK, w.Code)
 }
 
-func TestSampleRoute_ShouldReturnInvalidRequestWhenNoDataGiven(t *testing.T) {
-	var config = RouteHandler{}
-	r := createRouter(&config)
+type MockEmailer struct {
+	mock.Mock
+}
+
+func (e MockEmailer) SendEmail(d *EmailDetails) error {
+	args := e.Called(d)
+	return args.Error(0)
+}
+
+func TestSampleRoute_ShouldReturnInvalidRequestWhenNoBody(t *testing.T) {
+	var handler = RouteHandler{}
+	r := createRouter(&handler)
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("POST", "/companies/sample", nil)
@@ -84,13 +84,41 @@ func TestSampleRoute_ShouldReturnInvalidRequestWhenNoDataGiven(t *testing.T) {
 	assert.Equal(t, 400, w.Code)
 }
 
-func TestSampleRoute_ShouldReturnInvalidRequestWhenIndustryDoesNotExist(t *testing.T) {
-	var config = RouteHandler{}
-	r := createRouter(&config)
+func TestSampleRoute_ShouldReturnInvalidRequestWhenInvalidJsonBody(t *testing.T) {
+	var handler = RouteHandler{}
+	r := createRouter(&handler)
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("POST", "/companies/sample", bytes.NewReader([]byte("hello world")))
 	r.ServeHTTP(w, req)
 
 	assert.Equal(t, 400, w.Code)
+}
+
+func TestSampleRoute_ShouldReturnData(t *testing.T) {
+	d := MockDatabase{}
+
+	industry := "Extraction of salt"
+	d.On("GetSampleListOfCompaniesForIndustry", &industry).Return(&[]ProcessedCompany{{}})
+
+	handler := RouteHandler{
+		Database: d,
+	}
+
+	r := createRouter(&handler)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/companies/sample", bytes.NewReader([]byte(`{"SicDescription":"Extraction of salt"}`)))
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, 400, w.Code)
+}
+
+type MockDatabase struct {
+	mock.Mock
+}
+
+func (e MockDatabase) GetSampleListOfCompaniesForIndustry(i *string) (*[]ProcessedCompany, error) {
+	args := e.Called(i)
+	return args.Get(0).(*[]ProcessedCompany), args.Error(1)
 }
